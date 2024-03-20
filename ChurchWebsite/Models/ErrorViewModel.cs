dae1c30c011912167146a699a718a1c81c5793d1
Lian Hong <lbhong@captechu.edu>
2024-03-19 21:55:07 -0400

    Add project files.

---
+++ ChurchWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChurchWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
